7052a09ba647f9376803429f12d617d4d663c063
Gennie Nguyen <u7616055@anu.edu.au>
2024-04-30 23:39:17 +1000

    UI = User profile

--- app/src/main/java/com/anu/gp24s1/ui/following/FollowingViewModel.java
+++ app/src/main/java/com/anu/gp24s1/ui/following/FollowingViewModel.java
@@ -1,14 +1,14 @@
-package com.anu.gp24s1.ui.dashboard;
+package com.anu.gp24s1.ui.following;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class FollowingViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public FollowingViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
     }
