be5726e93c84e9d8c32cc86266109d0d54ec72bc
LachlanJS <43869608+LachlanJS@users.noreply.github.com>
2024-05-06 10:33:25 +1000

    Working on connecting the search frontend to the backend

--- app/src/main/java/com/anu/gp24s1/ui/search/SearchFragment.java
+++ app/src/main/java/com/anu/gp24s1/ui/search/SearchFragment.java
@@ -10,11 +10,13 @@ import androidx.fragment.app.Fragment;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.MultiAutoCompleteTextView;
+import android.widget.TextView;
 
 import com.anu.gp24s1.MainActivity;
 import com.anu.gp24s1.R;
@@ -43,6 +45,28 @@ public class SearchFragment extends Fragment {
         searchBinding = FragmentSearchBinding.inflate(inflater, container, false);
 
         View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        // Add listeners to the searchViewModel data:
+        searchViewModel.getTitleState().observe(getViewLifecycleOwner(), title -> {
+            // TODO: update the result based on the title:
+            // NOTE: title is a String
+
+
+        });
+
+        // Add listeners to the searchViewModel data:
+        searchViewModel.getLocationsState().observe(getViewLifecycleOwner(), locations -> {
+            // TODO: update the result based on the locations:
+            // NOTE: locations is a set
+
+        });
+
+        // Add listeners to the searchViewModel data:
+        searchViewModel.getTagsState().observe(getViewLifecycleOwner(), tags -> {
+            // TODO: update the result based on the locations:
+            // NOTE: tags is a set
+
+        });
 
         // get existing tags and locations:
         Set<String> tagsSet = PostDaoImpl.getInstance().getAllTags();
@@ -73,21 +97,15 @@ public class SearchFragment extends Fragment {
 
         searchContent.setThreshold(1);
         searchContent.setAdapter(autoCompletionArrayAdapter);
-        searchContent.addTextChangedListener(new TextWatcher(){
-
+        searchContent.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                searchViewModel.updateSearch(editable.toString());
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+                    if (keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                        searchViewModel.updateSearch(textView.toString());
+                    }
+                }
+                return false;
             }
         });
 
