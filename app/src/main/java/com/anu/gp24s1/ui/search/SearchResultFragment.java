2d2c34c9cf920a0244fa63c8d1d7a6b3e53ba4c7
Qinjue Wu <u7776887@anu.edu.au>
2024-05-09 13:24:02 +1000

    complete search

--- app/src/main/java/com/anu/gp24s1/ui/search/SearchResultFragment.java
+++ app/src/main/java/com/anu/gp24s1/ui/search/SearchResultFragment.java
@@ -11,7 +11,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.anu.gp24s1.R;
+import com.anu.gp24s1.pojo.vo.PostVo;
+import com.anu.gp24s1.state.UserSession;
 import com.anu.gp24s1.ui.post.PostListFragment;
+import com.anu.gp24s1.ui.post.PostListViewModel;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,20 +25,21 @@ import com.anu.gp24s1.ui.post.PostListFragment;
  */
 public class SearchResultFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    SearchViewModel searchViewModel = null;
-
-    public SearchResultFragment() {
-        // Required empty public constructor
-    }
+    private PostListViewModel searchViewModel;
+//    // TODO: Rename parameter arguments, choose names that match
+//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+//
+//    // TODO: Rename and change types of parameters
+//    private String mParam1;
+//    private String mParam2;
+//
+//    SearchViewModel searchViewModel = null;
+//
+//    public SearchResultFragment() {
+//        // Required empty public constructor
+//    }
 
     /**
      * Use this factory method to create a new instance of
@@ -44,33 +50,38 @@ public class SearchResultFragment extends Fragment {
      * @return A new instance of fragment SearchResultFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SearchResultFragment newInstance(String param1, String param2) {
-        SearchResultFragment fragment = new SearchResultFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+//    public static SearchResultFragment newInstance(String param1, String param2) {
+//        SearchResultFragment fragment = new SearchResultFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        searchViewModel = new ViewModelProvider(requireActivity()).get(SearchViewModel.class);
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-    }
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+////        searchViewModel = new ViewModelProvider(requireActivity()).get(SearchViewModel.class);
+////
+////        if (getArguments() != null) {
+////            mParam1 = getArguments().getString(ARG_PARAM1);
+////            mParam2 = getArguments().getString(ARG_PARAM2);
+////        }
+//
+//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search_result, container, false);
+
+        searchViewModel = new ViewModelProvider(requireActivity()).get(PostListViewModel.class);
+        String searchWords = getArguments().getString("searchWords");
+        List<PostVo> postVos = UserSession.getInstance().searchPosts(searchWords);
+        searchViewModel.setPostVoListData(postVos);
 
         // Check if the fragment is already added, to avoid overlapping fragments on re-creating the view
         if (getChildFragmentManager().findFragmentById(R.id.searchResult) == null) {
